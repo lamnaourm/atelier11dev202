5e2e8675ef0dc99c7cd72f3c11b68beb0794b2d1
lamnaourm <mohammed.lamnaour@gmail.com>
2023-10-24 12:49:33 +0100

    posts

--- src/components/ListUser.jsx
+++ src/components/ListUser.jsx
@@ -5,16 +5,32 @@ export default class ListUser extends Component {
 
     state = {
         users: [],
+        posts:[],
+        activeId:0,
     }
     render() {
         return (
             <div className='list'>
                 <h1>Nombre d'utilisateurs : {this.state.users.length}</h1>
                 {
-                    this.state.users.map(u => <div className='user'>
+                    this.state.users.map(u => <div key={u.id} className='user'>
                         <h2>Nom : {u.name}</h2>
                         <p>Email : {u.email}</p>
                         <p>Ville : {u.address.city} - Rue : {u.address.street}</p>
+                        <button onClick={() => this.setState({activeId: u.id})}>Details posts</button>
+
+                        {
+                            this.state.activeId === u.id && <div className='posts'>
+                                <h1>Nombre de posts : {this.state.posts.length}</h1>
+                                {
+                                    this.state.posts.map(p => <div className='post'>
+                                        <h3>{p.title}</h3>
+                                        <p>{p.body}</p>
+                                    </div>
+                                        )
+                                }
+                            </div>
+                        }
                     </div>
                     )
                 }
@@ -29,5 +45,16 @@ export default class ListUser extends Component {
         }
 
         getData().then(users => this.setState({ users }));
+    }
+
+    componentDidUpdate(prevprops, prevstate) {
+        if(prevstate.activeId !== this.state.activeId){
+            const getData = async () => {
+                const res = await axios.get('https://jsonplaceholder.typicode.com/posts?userId='+this.state.activeId);
+                return res.data
+            }
+    
+            getData().then(posts => this.setState({ posts }));
+        }
     }
 }
